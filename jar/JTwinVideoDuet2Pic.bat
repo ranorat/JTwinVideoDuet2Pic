d23a76f8b175fce3582af3296ae3fe2159f72e78
ranorat <engeitomato@gmail.com>
2026-08-16 21:26:11 +0900

    Add files via upload

--- jar/JTwinVideoDuet2Pic.bat
+++ jar/JTwinVideoDuet2Pic.bat
@@ -1,1 +1,8 @@
+@echo off
 
+cd /d %~dp0
+echo %cd%
+
+java --module-path ".\javafx-sdk\lib" --add-modules javafx.controls,javafx.media,javafx.swing --enable-native-access=javafx.graphics,javafx.media -jar .\JTwinVideoDuet2Pic.jar
+
+pause
